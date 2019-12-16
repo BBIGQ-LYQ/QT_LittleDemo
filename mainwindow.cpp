dc8cb25fc1430581fe6791693ebb5c0936b16f9e
BBIGQ-LYQ <1045042619@qq.com>
2019-12-16 17:29:02 +0800

    filrt commit

---
+++ mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_ENTER_clicked()
+{
+    num ++;
+    QString msg;
+    msg.sprintf("touch time is : %d ",num);
+
+    ui->RESULT->setText(msg);
+
+}
